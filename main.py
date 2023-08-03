4958fb562c6f224bb2e04e1677a5897b82ec921b
brunodos-santos <bruno6.santos@usp.br>
2023-08-03 17:41:49 -0300

    E-mail de parabenização teste

--- main.py
+++ main.py
@@ -1,45 +1,32 @@
-import smtplib
-import datetime as dt
-from random import choice
+from datetime import datetime
+import pandas as pd
+from random import randint
+from smtplib import SMTP
 
 MY_EMAIL = "brunocool719@gmail.com"
 MY_PASSWORD = "nbloqgodrucgiuig"
 
-now = dt.datetime.now()
-weekday = now.weekday()
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-# If the week day is equals to monday
-if weekday == 3:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = choice(all_quotes)
+data = pd.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-    print(quote)
-    with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+# HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
+# if (today_month, today_day) in birthdays_dict:
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{randint(1,3)}.txt"
+
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with SMTP(host="smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs=MY_EMAIL,
-            msg=f"Subject:Monday Motivation\n\n{quote}"
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy birthday {birthday_person['name']}!\n\n{contents}"
         )
-
-#     with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
-#         connection
-# my_email = "brunocool719@gmail.com"
-# password = "nbloqgodrucgiuig"
-# receiver_email = "bruno.santos@elogroup.com.br"
-#
-# with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs=receiver_email,
-#         msg="Subject: Hello bro\n\n"
-#             "This is the body of my email."
-#     )
-#
-#
-# date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
-# print(date_of_birth)
